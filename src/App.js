ec9550d5820a8734937888db75c96b0b5faf2fb4
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2021-06-20 16:10:30 -0500

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>FELIZ DIA DEL PADRE!</h1>
+
+      <img
+        className="dark"
+        src="https://i.pinimg.com/564x/08/eb/5e/08eb5e9465ab3d65f406f852e5e35ad6.jpg"
+        alt="imagen"
+      />
+      <Link to="/cards" className="bn31" href="/">
+        <span className="bn31span">Sigue</span>
+      </Link>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Route, HashRouter as Router } from "react-router-dom";
+import cards from "./componets/cards";
+import card from "./componets/card";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Route exact path="/" component={App} />
+    <Route path="/cards" component={cards} />
+    <Route path="/cards" component={card} />
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
